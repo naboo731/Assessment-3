e4ed53cf24e15e601f78293da3f4a60553e0bb74
Nichole Boothman <nbooth731@gmail.com>
2021-11-27 12:36:19 -0500

    Complete

---
+++ neighborhood.js
@@ -0,0 +1,13 @@
+const randomBtn = document.querySelector("#random-btn")
+const crookedHam = document.querySelector("#crooked-ham")
+const cantwell = document.querySelector("#cantwell")
+const twoStones = document.querySelector("#two-stones")
+const metroPub = document.querySelector("#metro")
+const sushiYama = document.querySelector("#sushi-yama")
+const carusos = document.querySelector("#carusos")
+const restArray = [crookedHam, cantwell, twoStones, metroPub, sushiYama, carusos]
+
+randomBtn.addEventListener("click", () => {
+    const random = Math.floor(Math.random() * restArray.length)
+    console.log(random, restArray[random])
+})
