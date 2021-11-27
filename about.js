40497fe4292f5703c99beaa173398648ec481866
Nichole Boothman <nbooth731@gmail.com>
2021-11-27 11:09:33 -0500

    Complete

--- about.js
+++ about.js
@@ -5,10 +5,15 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form Submitted Successfully');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let imgCompliment = document.querySelector("#rubber-duck-image")
+imgCompliment.addEventListener('mouseover', () => {
+	alert("Hey there, good looking!")
+})
---
+++ profile.js
@@ -0,0 +1,15 @@
+const colorButton = document.querySelector("#color")
+const placeButton = document.querySelector("#place")
+const ritualButton = document.querySelector("#ritual")
+
+colorButton.addEventListener('click', () => {
+    alert("Pale yellow!")
+})
+
+placeButton.addEventListener('click', () => {
+    alert("Home with family")
+})
+
+ritualButton.addEventListener('click', () => {
+    alert("Going to Wawa for breakfast and coffee before church on Sundays")
+})
